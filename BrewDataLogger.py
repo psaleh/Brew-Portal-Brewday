7e8d3c6190402610686d8c0a2022f0ce03ce08d5
Paul Saleh <drpaulsaleh@gmail.com>
2017-11-13 10:30:39 +0800

    initial commit

---
+++ BrewDataLogger.py
@@ -0,0 +1,37 @@
+import socket
+import json
+from datetime import datetime
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+
+def mongoinsert(jdata):
+    # connect to MongoDB
+    client = MongoClient("mongodb://brewportal_db_1")
+    db = client.meteor
+    db.brewdata.insert_one(jdata)
+    # Issue the serverStatus command and print the results
+    #serverStatusResult=db.command("serverStatus")
+    #pprint(serverStatusResult)
+
+if __name__ == '__main__':
+   s = socket.socket()         # Create a socket object
+   host = socket.gethostname() # Get local machine name
+   port = 12347                # Reserve a port for your service.
+   s.bind((host, port))        # Bind to the port
+
+   s.listen(5)                 # Now wait for client connection.
+   while True:
+      c, addr = s.accept()     # Establish connection with client.
+      print 'Got connection from', addr
+      data = c.recv(1024)
+      jdata = json.loads(data.decode('utf-8'))
+      jdata['timeStamp'] = datetime.utcnow()
+      mongoinsert(jdata)
+# print received data to stdout
+      print jdata['Colour']
+      print jdata['Beer']
+      print jdata['Temp']
+      print jdata['SG']
+      c.send('Data Received')
+      c.close()
